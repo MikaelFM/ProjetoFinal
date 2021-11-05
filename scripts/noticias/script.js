d8d985bdba115752cccef7d10d488fd867ac03ec
MikaelFM <mikaelfernandesmoreira@gmail.com>
2021-11-04 23:25:33 -0300

    botao

--- scripts/noticias/script.js
+++ scripts/noticias/script.js
@@ -1,10 +1,9 @@
 var x = 0
-setTimeout(() => {myFunction()}, 800);
 window.addEventListener('scroll', function () {
     scroll()
 })
-// ve se o scroll esta no inicio ou nao. Se tiver, chama a função navbar(), se não, chama a função padrao().
-const menu = function () {
+// ve se o scroll esta no inicio ou nao. Se tiver, chama a função navbar(), se não, chama a função padrao(). funcoes nao usadas neste site, mas mantidas para evitar erros.
+const menu = function () { /* faz com que o menu abra */
     if (window.matchMedia("(max-width:1470px) and (min-width:611px)").matches) {
         navbar()
         console.log('a viewport tem pelo menos 800 pixels de largura')
@@ -12,7 +11,7 @@ const menu = function () {
     var menu = document.getElementById('menu').style;
     menu.left = "0"
 
-}
+} /* fecha o menu */
 const fechar = function () {
     scroll()
     var menu = document.getElementById('menu').style;
@@ -22,6 +21,6 @@ function open(link){
     alert(link)
     links = ["https://centralvingadores.com.br/poster-de-what-if-revela-chegada-de-tchalla-como-senhor-das-estrelas/", "https://centralvingadores.com.br/trailer-de-homem-aranha-sem-volta-para-casa-assista/"]
     alert(links[link])
-    window.location = links[link];
+    window.location.href = links[link];
     target = "_blank";
 }
